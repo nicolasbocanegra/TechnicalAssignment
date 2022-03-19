43ad88f0641422f9de61f2e6ba3d5398edc2b5e2
Nicolás Bocanegra León <nicolasbocanegra@icloud.com>
2022-03-19 07:56:57 -0400

    Initial Commit

---
+++ Technical Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Technical Assignment
+//
+//  Created by Nicolás Bocanegra León on 3/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
